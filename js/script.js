0fdae533338117bea2ea063d2c585da0c6af5521
tomaz versolato carballo <RM551417@fiap.com.br>
2023-08-29 16:25:35 -0300

    Atualizado Ex Final.

--- js/script.js
+++ js/script.js
@@ -26,3 +26,71 @@ console.log(valoresSelecionados);
 const somaValores = valoresSelecionados.reduce((acumulado, atual) => acumulado + atual);
 
 console.log(somaValores);
+
+// Exercíocio final:
+
+const formCadastro = document.querySelector("#formCadastro");
+const tabelas = document.querySelector("#tabelas");
+const ordenar = document.querySelector("#ordenar");
+
+let listaCadastro = [];
+
+formCadastro.addEventListener("submit", function(evento) {
+    evento.preventDefault();
+
+    const inDescricao = document.querySelector("#idDescricao").value;
+    const inAutor = document.querySelector("#idAutor").value;
+    const inDepartamento = document.querySelector("#idDepartamento").value;
+    const inImportancia = document.querySelector("#idImportancia").value;
+    const inValor = document.querySelector("#idValor").value;
+    const inDuracao = document.querySelector("#idDuracao").value;
+
+    const cadastro = {
+        inDescricao,
+        inAutor,
+        inDepartamento,
+        inImportancia,
+        inValor,
+        inDuracao,
+    };
+
+    listaCadastro.push(cadastro);
+    atualizar();
+    formCadastro.reset();
+});
+
+function atualizar(){
+
+tabelas.innerHTML = `
+    <tr>
+    <th>Descrição</th>
+    <th>Autor</th>
+    <th>Departamento</th>
+    <th>Importância</th>
+    <th>Valor</th>
+    <th>Duração</th>
+    <th></th>
+    </tr>
+    ${listaCadastro.map((cadastro, index) => `
+        <tr>
+            <td>${cadastro.inDescricao}</td>
+            <td>${cadastro.inAutor}</td>
+            <td>${cadastro.inDepartamento}</td>
+            <td>${cadastro.inImportancia}</td>
+            <td>${cadastro.inValor || ""}</td>
+            <td>${cadastro.inDuracao || ""}</td>
+            <td><button onclick="removeCadastro(${index})">Excluir</button></td>
+        </tr>    
+        `).join("")}
+    `;
+}
+
+function removeCadastro(index){
+    listaCadastro.splice(index, 1);
+    atualizar();
+}
+
+ordenar.addEventListener("click", function () {
+    listaCadastro.sort((a, b) => b.inImportancia - a.inImportancia);
+    atualizar();
+});
