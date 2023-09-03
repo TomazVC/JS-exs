b32cbabe273df88b4e7d370145b34e5144650c81
tomaz versolato carballo <RM551417@fiap.com.br>
2023-09-02 23:32:50 -0300

    Pronto.

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ formCadastro.addEventListener("submit", function(evento) {
     const inValor = document.querySelector("#idValor").value;
     const inDuracao = document.querySelector("#idDuracao").value;
 
-    const cadastro = {
+    const cadastro = {//cria um objeto que armazena os valores colocados, como um dicionario em python
         inDescricao,
         inAutor,
         inDepartamento,
@@ -54,13 +54,22 @@ formCadastro.addEventListener("submit", function(evento) {
         inDuracao,
     };
 
-    listaCadastro.push(cadastro);
-    atualizar();
-    formCadastro.reset();
+    listaCadastro.push(cadastro); //vai inserir as cadastros na lista
+    atualizar(); //vai dar update no html
+    formCadastro.reset(); //vai resetar para inserir novos cadastros
 });
 
 function atualizar(){
-
+    //substitui a tabela atual por uma nova tabela, estruturada no html
+    //${} permite que eu insira novas expressoes js dentro da string
+    //<tr> cria a linha de cabeçalho da tabela com os titulos das colunas
+    //listaCadastro.map itera sobre a matriz de tarefas (listaCadastro) e cria uma nova linha HTML para cada tarefa
+    //${cadastro.inDescricao}, ${cadastro.inAutor}, etc.: usado para as propriedades de cada objeto preencher as células da tabela
+    //${cadastro.inValor || ""} e ${cadastro.inDuracao || ""}: é usado o operador || para fornecer um valor padrão (no caso, uma string vazia "")
+    //caso o valor de cadastro.inValor ou cadastro.inDuracao seja falso (como null, undefined ou false)
+    //<button onclick="removeCadastro(${index})">Excluir</button>: é criado um botão "Excluir" em cada linha da tabela
+    //Quando esse botão é clicado, ele chama a função removeCadastro(index) para remover a tarefa correspondente da matriz
+    //.join(""): A função map cria um array de strings (uma para cada linha da tabela). O .join("") é usado para juntar essas strings em um único bloco de HTML
 tabelas.innerHTML = `
     <tr>
     <th>Descrição</th>
@@ -84,12 +93,15 @@ tabelas.innerHTML = `
         `).join("")}
     `;
 }
-
+//listaCadastro.splice(index, 1): A função splice é usada para modificar uma matriz, removendo ou substituindo elementos
+//estou removendo um elemento da matriz listaCadastro com base no índice index. O segundo argumento 1 indica que queremos remover um único elemento a partir desse índice
+//Após remover a tarefa da matriz, chamo a função atualizar() para atualizar a tabela na página, refletindo as alterações feitas na matriz
 function removeCadastro(index){
     listaCadastro.splice(index, 1);
     atualizar();
 }
-
+//Dentro dessa função anônima, estamos usando o método sort da matriz para ordenar as tarefas com base em sua importância (inImportancia)
+//A função de comparação ((a, b) => b.inImportancia - a.inImportancia) classifica as tarefas em ordem decrescente de importância
 ordenar.addEventListener("click", function () {
     listaCadastro.sort((a, b) => b.inImportancia - a.inImportancia);
     atualizar();
